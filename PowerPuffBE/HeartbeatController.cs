3c739f9732224a20173c3cb4a78d7106b74fbb0a
Rafal Walczak <rafal.walczak@vattenfall.com>
2024-07-25 11:48:57 +0200

    Added HeartBeat endpoint

---
+++ PowerPuffBE/HeartbeatController.cs
@@ -0,0 +1,25 @@
+﻿namespace PowerPuffBE;
+
+using Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service.Services;
+
+
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class HeartbeatController : ControllerBase
+{
+
+    public HeartbeatController()
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Beat()
+    {
+        return Ok("Tłusty Beat");
+    }
+}
